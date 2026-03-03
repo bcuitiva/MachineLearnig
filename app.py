ba0ad5c0d6412fe31bfc298a5efc506ea79b82a5
Elkin Almonacid <elkinyamith13@gmail.com>
2026-03-02 23:05:03 -0500

    Agrego app.py y estructura

--- app.py
+++ app.py
@@ -5,15 +5,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello Flask"
-
-@app.route('/FirstPage')
-def firstPage():
-    return render_template('index.html')
-
-@app.route('/CasoUso')
-def casoUso():
-    return render_template('casoUso.html')
-
-@app.route('/UseCase')
-def UseCase():
-    return render_template('UseCase.html')
